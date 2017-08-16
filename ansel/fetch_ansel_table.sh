a3cfadcb08926e8cfe96eebe428e1cb849b63a40
Christopher A. Mosher <cmosher01@gmail.com>
2017-08-16 00:15:49 -0400

    add 6 extra special GEDCOM ANSEL characters

--- ansel/fetch_ansel_table.sh
+++ ansel/fetch_ansel_table.sh
@@ -8,4 +8,13 @@
 
 url=http://www.loc.gov/marc/specifications/codetables.xml
 xpath='/codeTables/codeTable[contains(@name,"Latin")]/characterSet[contains(@name,"ANSEL")]/code'
-curl -s $url | xmlstarlet sel -t -m "$xpath" -v "marc" -o "," -v "ucs" -n >ansel.csv
+curl -s "$url" | xmlstarlet sel -t -m "$xpath" -v "marc" -o "," -v "ucs" -n >ansel.csv
+
+# Adds some additional values from various GEDCOM (de-facto) standards:
+
+echo "BE,25A1" >>ansel.csv
+echo "BF,25A0" >>ansel.csv
+echo "CD,0065" >>ansel.csv
+echo "CE,006F" >>ansel.csv
+echo "CF,00DF" >>ansel.csv
+echo "FC,0338" >>ansel.csv
